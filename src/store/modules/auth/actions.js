f203cd5e27fba47aba158c9b9bab22bc77ae26da
Francuski Miroslav <francuski.miroslav@gmail.com>
2023-02-09 12:20:40 +0100

    added login action to firebase api

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,6 +1,28 @@
 export default {
-    login() {
+    async login(context, payload) {
+        const apiKey = 'AIzaSyCk-leJXZaAZZS4UFhwHXUzFY4sFGltzw8'
+        const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, {
+            method: 'POST',
+            body: JSON.stringify({
+                email: payload.email,
+                password: payload.password,
+                returnSecureToken: true
+            })
+        });
 
+        const responseData = await response.json();
+
+        if(!response.ok) {
+            const error = new Error(responseData.message || 'Login invalid, failed to authenticate. Please check your login data')
+            throw error;
+        };
+
+        console.log(responseData)
+        context.commit('setUser', {
+            token: responseData.idToken,
+            userId: responseData.localId,
+            tokenExpiration: responseData.expiresIn
+        })
     },
 
     async signup(context, payload) {
