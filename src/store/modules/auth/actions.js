e49a8444de834a703519c7a658a44184bd383c1f
Francuski Miroslav <francuski.miroslav@gmail.com>
2023-02-09 12:00:39 +0100

    added signup action to firebase api

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,3 +1,31 @@
 export default {
+    login() {
 
+    },
+
+    async signup(context, payload) {
+        const apiKey = 'AIzaSyCk-leJXZaAZZS4UFhwHXUzFY4sFGltzw8'
+        const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`, {
+            method: 'POST',
+            body: JSON.stringify({
+                email: payload.email,
+                password: payload.password,
+                returnSecureToken: true
+            })
+        });
+
+        const responseData = await response.json();
+
+        if(!response.ok) {
+            const error = new Error(responseData.message || 'Signup invalid, failed to authenticate.')
+            throw error;
+        };
+
+        console.log(responseData)
+        context.commit('setUser', {
+            token: responseData.idToken,
+            userId: responseData.localId,
+            tokenExpiration: responseData.expiresIn
+        })
+    },
 };
